9d15d1e4e5861b9b065c544837e45b2f39447210
AndreCox <arc.xoc@gmail.com>
2023-11-13 04:58:59 +0000

    Deploying to gh-pages from @ AndreCox/eom-solver@89bc01d97985900a992b4b282da3d4bf1ce68c33 🚀

---
+++ _next/static/epsbl47JrZAAFQmKiL4Gj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/ee8b1517-ef7d789731570c8e.js","static/chunks/219-ddf608bb48ef6ef2.js","static/chunks/pages/index-4517b08055e9014b.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
